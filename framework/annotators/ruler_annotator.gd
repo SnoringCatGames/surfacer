05209319a775723e008fa42011cedae4908d60f5
Levi Lindsey <levi@levi.codes>
2019-10-20 07:52:39 -0700

    Fix ruler annotator to work with camera zoom.

--- framework/annotators/ruler_annotator.gd
+++ framework/annotators/ruler_annotator.gd
@@ -31,16 +31,17 @@ func _process(delta: float) -> void:
         update()
 
 func _draw() -> void:
-    var screen_start_position := screen_center - viewport_size / 2.0
+    var grid_spacing := GRID_SPACING / Global.CAMERA_ZOOM
+    var screen_start_position := screen_center / Global.CAMERA_ZOOM - viewport_size / 2.0
     
     # Offset the start position to align with the grid cell boundaries.
     var ruler_start_position := Vector2( \
-            -fmod((screen_start_position.x + GRID_SPACING * 1000000000), GRID_SPACING), \
-            -fmod((screen_start_position.y + GRID_SPACING * 1000000000), GRID_SPACING))
+            -fmod((screen_start_position.x + grid_spacing * 1000000000), grid_spacing), \
+            -fmod((screen_start_position.y + grid_spacing * 1000000000), grid_spacing))
     
-    var ruler_size := viewport_size + Vector2(GRID_SPACING, GRID_SPACING)
-    var vertical_line_count := floor(ruler_size.x / GRID_SPACING) as int + 1
-    var horizontal_line_count := floor(ruler_size.y / GRID_SPACING) as int + 1
+    var ruler_size := viewport_size + Vector2(grid_spacing, grid_spacing)
+    var vertical_line_count := floor(ruler_size.x / grid_spacing) as int + 1
+    var horizontal_line_count := floor(ruler_size.y / grid_spacing) as int + 1
     
     var label := Label.new()
     var font := label.get_font("")
@@ -54,23 +55,25 @@ func _draw() -> void:
     # Draw the vertical lines.
     start_y = ruler_start_position.y
     for i in range(vertical_line_count):
-        start_x = ruler_start_position.x + GRID_SPACING * i
+        start_x = ruler_start_position.x + grid_spacing * i
         start_position = Vector2(start_x, start_y)
         end_position = Vector2(start_x, start_y + ruler_size.y)
         draw_line(start_position, end_position, LINE_COLOR, LINE_WIDTH)
         
-        text = str(floor(screen_start_position.x + start_x))
+        text = str(round((screen_start_position.x + start_x) * Global.CAMERA_ZOOM))
+        text = "0" if text == "-0" else text
         draw_string(font, Vector2(start_position.x + 2, 14), text, TEXT_COLOR)
     
     # Draw the horizontal lines.
     start_x = ruler_start_position.x
     for i in range(1, horizontal_line_count):
-        start_y = ruler_start_position.y + GRID_SPACING * i
+        start_y = ruler_start_position.y + grid_spacing * i
         start_position = Vector2(start_x, start_y)
         end_position = Vector2(start_x + ruler_size.x, start_y)
         draw_line(start_position, end_position, LINE_COLOR, LINE_WIDTH)
         
-        text = str(floor(screen_start_position.y + start_y))
+        text = str(round((screen_start_position.y + start_y) * Global.CAMERA_ZOOM))
+        text = "0" if text == "-0" else text
         draw_string(font, Vector2(2, start_position.y + 14), text, TEXT_COLOR)
     
     label.free()
