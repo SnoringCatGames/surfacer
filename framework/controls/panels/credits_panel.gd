d1e126fc036e5d84799ae2d8533fafa08fbc4e78
Levi Lindsey <levi@levi.codes>
2020-05-06 09:08:18 -0700

    Add links to the credits panel.

--- framework/controls/panels/credits_panel.gd
+++ framework/controls/panels/credits_panel.gd
@@ -1,5 +1,14 @@
 extends WindowDialog
 class_name CreditsPanel
 
+const LEVI_URL := "https://levi.dev"
+const GODOT_URL := "https://godotengine.org"
+
 func _on_third_party_licenses_button_pressed():
     $ThirdPartyLicensesPanel.popup()
+
+func _on_levi_link_pressed():
+    OS.shell_open(LEVI_URL)
+
+func _on_godot_link_pressed():
+    OS.shell_open(GODOT_URL)
