2c0edfac1a2dba373eef84375bf7094984203c85
Levi Lindsey <levi@levi.codes>
2020-05-28 08:13:26 -0700

    Be more consistent with plural folder names.

--- framework/annotators/annotation_elements/annotation_element.gd
+++ framework/annotators/annotation_elements/annotation_element.gd
@@ -9,7 +9,11 @@ func _init(type: int) -> void:
 func draw(canvas: CanvasItem) -> void:
     Utils.error("Abstract AnnotationElement.draw is not implemented")
 
+func get_legend_items() -> Array:
+    Utils.error( \
+            "Abstract AnnotationElement.get_legend_items is not implemented")
+    return []
+
 func clear() -> void:
     # Do nothing unless the sub-class implements this.
     pass
-
