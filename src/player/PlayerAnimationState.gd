c9b6f137dc62baa4f8327ae4d3164c613e1c5079
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-05-11 20:20:00 -0700

    Create a new PlayerAnimationType enum.

--- src/player/PlayerAnimationState.gd
+++ src/player/PlayerAnimationState.gd
@@ -2,10 +2,10 @@ class_name PlayerAnimationState
 extends Reference
 
 var player_position := Vector2.INF
-var animation_name := ""
+var animation_type := PlayerAnimationType.UNKNOWN
 var animation_position := 0.0
 
 func reset() -> void:
     self.player_position = Vector2.INF
-    self.animation_name = ""
+    self.animation_type = PlayerAnimationType.UNKNOWN
     self.animation_position = 0.0
