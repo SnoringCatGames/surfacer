585b5f335e3e489c818f8623b118671350d9d4e0
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-05-20 19:11:38 -0700

    Show an exclamation mark when a player starts a new navigation.

--- src/annotators/NavigatorAnnotator.gd
+++ src/annotators/NavigatorAnnotator.gd
@@ -3,6 +3,12 @@ extends Node2D
 
 var FADE_IN_DURATION := 0.35
 
+var EXCLAMATION_MARK_SCALE_START := Vector2(0.2, 0.2)
+var EXCLAMATION_MARK_SCALE_MIDDLE := Vector2(0.8, 0.8)
+var EXCLAMATION_MARK_SCALE_END := Vector2(1.5, 1.5)
+var EXCLAMATION_MARK_DURATION := 1.0
+var EXCLAMATION_MARK_VERTICAL_OFFSET := -32.0
+
 var navigator: Navigator
 var previous_path: PlatformGraphPath
 var current_path: PlatformGraphPath
@@ -16,8 +22,12 @@ var fade_progress := 0.0
 
 var previous_path_back_end_trim_radius: float
 
+var exclamation_mark_offset: Vector2
+
 var pulse_annotator: NavigationPulseAnnotator
-var tween: ScaffolderTween
+var exclamation_mark: Label
+var fade_tween: ScaffolderTween
+var exclamation_mark_tween: ScaffolderTween
 
 func _init(navigator: Navigator) -> void:
     self.navigator = navigator
@@ -28,19 +38,46 @@ func _init(navigator: Navigator) -> void:
     self.pulse_annotator = NavigationPulseAnnotator.new(navigator)
     add_child(pulse_annotator)
     
-    self.tween = ScaffolderTween.new()
-    add_child(tween)
+    self.fade_tween = ScaffolderTween.new()
+    add_child(fade_tween)
+    
+    self.exclamation_mark_tween = ScaffolderTween.new()
+    exclamation_mark_tween.connect(
+            "tween_all_completed", self, "_exclamation_mark_tween_completed")
+    add_child(exclamation_mark_tween)
+    
+    self.exclamation_mark = Label.new()
+    exclamation_mark.add_font_override("font", Gs.fonts.main_m)
+    var exclamation_mark_color: Color = \
+            Surfacer.ann_defaults \
+                    .HUMAN_NAVIGATOR_CURRENT_PATH_COLOR if \
+            navigator.player.is_human_player else \
+            Surfacer.ann_defaults.COMPUTER_NAVIGATOR_CURRENT_PATH_COLOR
+    exclamation_mark_color.a = 1.0
+    exclamation_mark.add_color_override("font_color", exclamation_mark_color)
+    exclamation_mark.align = Label.ALIGN_CENTER
+    exclamation_mark.valign = Label.VALIGN_CENTER
+    exclamation_mark.text = "!"
+    exclamation_mark.visible = false
+    add_child(exclamation_mark)
+    
+    _update_exclamation_mark_offset()
+    call_deferred("_update_exclamation_mark_offset")
 
 func _physics_process(_delta_sec: float) -> void:
     is_fade_in_progress = fade_progress != previous_fade_progress
     previous_fade_progress = fade_progress
     
+    exclamation_mark.rect_position = \
+            navigator.player.position + exclamation_mark_offset
+    
     if navigator.path != current_path:
         current_path = navigator.path
         current_destination = navigator.get_destination()
-        if current_path != null and \
-                is_enabled:
-            _trigger_fade_in(true)
+        if current_path != null:
+            _trigger_exclamation_mark()
+            if is_enabled:
+                _trigger_fade_in(true)
         update()
     
     if navigator.previous_path != previous_path:
@@ -56,9 +93,18 @@ func _physics_process(_delta_sec: float) -> void:
     if is_fade_in_progress:
         update()
 
+func _update_exclamation_mark_offset() -> void:
+    var half_size := exclamation_mark.rect_size / 2.0
+    exclamation_mark_offset = \
+            -half_size + Vector2(
+            0.0,
+            -navigator.player.movement_params.collider_half_width_height.y + \
+            EXCLAMATION_MARK_VERTICAL_OFFSET)
+    exclamation_mark.rect_pivot_offset = half_size
+
 func _trigger_fade_in(is_fade_in := true) -> void:
-    tween.stop_all()
-    tween.interpolate_property(
+    fade_tween.stop_all()
+    fade_tween.interpolate_property(
             self,
             "fade_progress",
             0.0 if is_fade_in else 1.0,
@@ -67,7 +113,53 @@ func _trigger_fade_in(is_fade_in := true) -> void:
             "ease_out",
             0.0,
             TimeType.PLAY_PHYSICS)
-    tween.start()
+    fade_tween.start()
+
+func _trigger_exclamation_mark() -> void:
+    var opacity_start := 1.0
+    var opacity_end := 0.0
+    
+    var scale_delay := EXCLAMATION_MARK_DURATION * 0.1
+    var scale_duration := EXCLAMATION_MARK_DURATION - scale_delay
+    var opacity_delay := EXCLAMATION_MARK_DURATION * 0.3
+    var opacity_duration := EXCLAMATION_MARK_DURATION - opacity_delay
+    
+    exclamation_mark.visible = true
+    exclamation_mark.rect_scale = EXCLAMATION_MARK_SCALE_START
+    exclamation_mark.modulate.a = opacity_start
+    
+    exclamation_mark_tween.stop_all()
+    exclamation_mark_tween.interpolate_property(
+            exclamation_mark,
+            "rect_scale",
+            EXCLAMATION_MARK_SCALE_START,
+            EXCLAMATION_MARK_SCALE_MIDDLE,
+            scale_delay,
+            "ease_out_strong",
+            0.0,
+            TimeType.PLAY_PHYSICS_SCALED)
+    exclamation_mark_tween.interpolate_property(
+            exclamation_mark,
+            "rect_scale",
+            EXCLAMATION_MARK_SCALE_MIDDLE,
+            EXCLAMATION_MARK_SCALE_END,
+            scale_duration,
+            "ease_out_strong",
+            scale_delay,
+            TimeType.PLAY_PHYSICS_SCALED)
+    exclamation_mark_tween.interpolate_property(
+            exclamation_mark,
+            "modulate:a",
+            opacity_start,
+            opacity_end,
+            opacity_duration,
+            "ease_out_strong",
+            opacity_delay,
+            TimeType.PLAY_PHYSICS_SCALED)
+    exclamation_mark_tween.start()
+
+func _exclamation_mark_tween_completed() -> void:
+    exclamation_mark.visible = false
 
 func _draw() -> void:
     if !is_enabled and \
