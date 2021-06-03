2da246da0c374326bc5f92fa11610d325307f5a4
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-06-03 15:07:09 -0700

    Remove long-obsolete tests.

--- test/test_old/unit/calculate_min_time_to_reach_position_test.gd
+++
@@ -1,9 +0,0 @@
-extends IntegrationTestBed
-
-# Expected values were calculated using a Google spreadsheet:
-# https://docs.google.com/spreadsheets/d/1qERIm_R-GjgmPqFgHa8GhI71gWRkXkX3Sy6FgSJNqrA/edit
-
-# FIXME
-
-func test_TODO() -> void:
-    pending()# FIXME
--- test/tests/placeholder_test.gd
+++
@@ -1,9 +0,0 @@
-extends IntegrationTestBed
-
-# TODO: Write more tests.
-
-func test_placeholder() -> void:
-    assert_eq(
-        true,
-        false,
-        "Placeholder")
