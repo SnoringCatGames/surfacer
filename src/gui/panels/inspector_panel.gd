6efbbadc211dbebe175dbc428cb2e230b82be406
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-07-13 14:15:23 -0700

    Support StyleBoxTexture for overlay panels.

--- src/gui/panels/inspector_panel.gd
+++ src/gui/panels/inspector_panel.gd
@@ -7,7 +7,7 @@ const PANEL_MARGIN_RIGHT := 20.0
 const TOGGLE_DURATION := 0.2
 const ANNOTATOR_ROW_HEIGHT := 21.0
 const CHECK_BOX_SCALE := 0.5
-const SLIDER_WIDTH := 72.0
+const SLIDER_WIDTH := 64.0
 
 var is_open := false setget _set_is_open
 var tree_font_size := "Xs" setget _set_tree_font_size
@@ -36,13 +36,13 @@ func _ready() -> void:
     if Engine.editor_hint:
         return
     
-    $PanelContainer/VBoxContainer/Header/XButtonWrapper/XButton \
+    $ScaffolderPanelContainer/VBoxContainer/Header/XButtonWrapper/XButton \
             .texture_pressed = Gs.icons.close_active
-    $PanelContainer/VBoxContainer/Header/XButtonWrapper/XButton \
+    $ScaffolderPanelContainer/VBoxContainer/Header/XButtonWrapper/XButton \
             .texture_hover = Gs.icons.close_hover
-    $PanelContainer/VBoxContainer/Header/XButtonWrapper/XButton \
+    $ScaffolderPanelContainer/VBoxContainer/Header/XButtonWrapper/XButton \
             .texture_normal = Gs.icons.close_normal
-    $PanelContainer/VBoxContainer/Header/XButtonWrapper/XButton \
+    $ScaffolderPanelContainer/VBoxContainer/Header/XButtonWrapper/XButton \
             .texture_scale = Vector2(2.0, 2.0)
     
     $Footer/GearButton.texture_pressed = Gs.icons.gear_circle_active
@@ -55,11 +55,6 @@ func _ready() -> void:
     $Footer/PauseButton.texture_normal = Gs.icons.pause_circle_normal
     $Footer/PauseButton.texture_scale = Vector2(4.0, 4.0)
     
-    # Make the panel style unique, so that we can change the border width
-    # without changing other things.
-    $PanelContainer.add_stylebox_override(
-            "panel", $PanelContainer.get_stylebox("panel").duplicate())
-    
     _set_tree_font_size(tree_font_size)
     
     Gs.gui.record_gui_original_size_recursively(self)
@@ -68,7 +63,9 @@ func _ready() -> void:
     
     theme = Gs.gui.theme
     
-    var x_button := $PanelContainer/VBoxContainer/Header/XButtonWrapper/XButton
+    var x_button := \
+            $ScaffolderPanelContainer/VBoxContainer/Header/XButtonWrapper/ \
+            XButton
     x_button.set_meta("gs_rect_position", x_button.rect_position)
     
     Gs.gui.add_gui_to_scale(self)
@@ -76,12 +73,13 @@ func _ready() -> void:
     _set_footer_visibility(!is_open)
     
     Surfacer.graph_inspector = \
-            $PanelContainer/VBoxContainer/Sections/InspectorContainer/ \
-            PlatformGraphInspector
+            $ScaffolderPanelContainer/VBoxContainer/Sections/ \
+            InspectorContainer/PlatformGraphInspector
     Surfacer.legend = \
-            $PanelContainer/VBoxContainer/Sections/Legend
+            $ScaffolderPanelContainer/VBoxContainer/Sections/Legend
     Surfacer.selection_description = \
-            $PanelContainer/VBoxContainer/Sections/SelectionDescription
+            $ScaffolderPanelContainer/VBoxContainer/Sections/ \
+            SelectionDescription
     
     if (Gs.gui.hud_manifest.inspector_panel_starts_open or \
                 Surfacer.debug_params.has("limit_parsing")) and \
@@ -114,7 +112,9 @@ func _on_gui_scale_changed() -> bool:
 
 
 func _deferred_on_gui_scale_changed() -> void:
-    var x_button := $PanelContainer/VBoxContainer/Header/XButtonWrapper/XButton
+    var x_button := \
+            $ScaffolderPanelContainer/VBoxContainer/Header/XButtonWrapper/ \
+            XButton
     x_button.rect_position = \
             x_button.get_meta("gs_rect_position") * Gs.gui.scale
     
@@ -122,7 +122,7 @@ func _deferred_on_gui_scale_changed() -> void:
         if child is Control:
             Gs.gui.scale_gui_recursively(child)
     
-    rect_size.x = $PanelContainer.rect_size.x
+    rect_size.x = $ScaffolderPanelContainer.rect_size.x
     
     _set_footer_visibility(!is_open)
     
@@ -135,28 +135,23 @@ func _deferred_on_gui_scale_changed() -> void:
             is_open else \
             _get_closed_position_y()
     
-    var border_color: Color = Gs.colors.overlay_panel_border
-    var border_width: float = \
-            Gs.styles.overlay_panel_border_width * Gs.gui.scale
-    
-    var panel_stylebox: StyleBoxFlat = $PanelContainer.get_stylebox("panel")
-    panel_stylebox.border_color = border_color
-    panel_stylebox.border_width_left = border_width
-    panel_stylebox.border_width_top = 0.0
-    panel_stylebox.border_width_right = border_width
-    panel_stylebox.border_width_bottom = border_width
+    var panel_stylebox: StyleBox = \
+            $ScaffolderPanelContainer.get_stylebox("panel")
+    if panel_stylebox is StyleBoxFlatScalable:
+        panel_stylebox.border_width_top = 0.0
     
     var separator_stylebox: StyleBoxLine = \
-            $PanelContainer/VBoxContainer/Sections/HSeparator \
+            $ScaffolderPanelContainer/VBoxContainer/Sections/HSeparator \
             .get_stylebox("separator")
-    separator_stylebox.color = border_color
-    separator_stylebox.thickness = border_width
+    separator_stylebox.color = Gs.colors.overlay_panel_border
+    separator_stylebox.thickness = Gs.styles.overlay_panel_border_width
 
 
 func _initialize_annotator_checkboxes() -> void:
     var empty_style := StyleBoxEmpty.new()
     var annotators := \
-            $PanelContainer/VBoxContainer/Sections/MarginContainer/Annotators
+            $ScaffolderPanelContainer/VBoxContainer/Sections/MarginContainer/ \
+            Annotators
     for child in annotators.get_children():
         child.queue_free()
     var item_width: float = \
@@ -195,7 +190,7 @@ func _initialize_annotator_checkboxes() -> void:
 
 
 func _get_closed_position_y() -> float:
-    return -$PanelContainer.rect_size.y - 1.0
+    return -$ScaffolderPanelContainer.rect_size.y - 1.0
 
 
 func _set_is_open(value: bool) -> void:
@@ -206,9 +201,9 @@ func _set_is_open(value: bool) -> void:
 func _set_tree_font_size(value: String) -> void:
     tree_font_size = value
     var font: Font = Gs.gui.get_font(tree_font_size)
-    $PanelContainer/VBoxContainer/Header/PauseButton \
+    $ScaffolderPanelContainer/VBoxContainer/Header/PauseButton \
             .add_font_override("font", font)
-    $PanelContainer/VBoxContainer/Sections/InspectorContainer/ \
+    $ScaffolderPanelContainer/VBoxContainer/Sections/InspectorContainer/ \
             PlatformGraphInspector \
             .add_font_override("font", font)
     for item in _annotator_control_items:
@@ -237,7 +232,7 @@ func _toggle_open() -> void:
             position_y_start,
             position_y_end,
             duration,
-            "ease_in")
+            "ease_out")
     
     _set_footer_visibility(!is_open)
     
@@ -257,11 +252,11 @@ func _set_footer_visibility(is_visible: bool) -> void:
     $Spacer.visible = is_visible
     $Footer.visible = is_visible
     rect_size.y = \
-            $PanelContainer.rect_size.y + \
+            $ScaffolderPanelContainer.rect_size.y + \
                     $Spacer.rect_size.y + \
                     $Footer.rect_size.y if \
             is_visible else \
-            $PanelContainer.rect_size.y
+            $ScaffolderPanelContainer.rect_size.y
 
 
 func _on_GearButton_pressed() -> void:
