7f9adb1040d3a43154224b69e519d147679db546
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-08-30 08:58:23 -0700

    Implement ClimbAdjacentSurfacesBehavior.

--- src/character/behaviors/climb_adjacent_surfaces_behavior.gd
+++ src/character/behaviors/climb_adjacent_surfaces_behavior.gd
@@ -14,6 +14,8 @@ const INCLUDES_MID_MOVEMENT_PAUSE := true
 const INCLUDES_POST_MOVEMENT_PAUSE := false
 const COULD_RETURN_TO_START_POSITION := false
 
+const DISTANCE_TO_SURFACE_END_THRESHOLD := 4.0
+
 ## -   If true, the character will climb in a clockwise direction around convex
 ##     corners.
 ##     -   I.e., the character will walk toward the right on floor surfaces.
@@ -34,8 +36,10 @@ func _init().(
     pass
 
 
-#func _on_active() -> void:
-#    ._on_active()
+func _on_active() -> void:
+    ._on_active()
+    if randomizes_initial_direction:
+        is_clockwise = randf() < 0.5
 
 
 #func _on_ready_to_move() -> void:
@@ -55,14 +59,11 @@ func _init().(
 
 
 func _move() -> bool:
-    # FIXME: LEFT OFF HERE: --------------------------
-    # - max_distance_from_start_position
-    # - start_position_for_max_distance_checks
-    # - _reached_max_distance
-    # - can_leave_start_surface
-    
-    if randomizes_initial_direction:
-        is_clockwise = randf() < 0.5
+    return _attempt_navigation(false)
+
+
+func _attempt_navigation(just_turned_around: bool) -> bool:
+    _reached_max_distance = false
     
     var next_surface := \
             start_surface.clockwise_neighbor if \
@@ -76,40 +77,102 @@ func _move() -> bool:
             (next_surface.side == SurfaceSide.LEFT_WALL or \
             next_surface.side == SurfaceSide.RIGHT_WALL) and \
             character.movement_params.can_grab_walls
+    var intra_surface_destination := _get_intra_surface_destination()
+    var is_already_at_surface_end := start_position.distance_squared_to( \
+                    intra_surface_destination.target_point) < \
+            DISTANCE_TO_SURFACE_END_THRESHOLD * \
+                    DISTANCE_TO_SURFACE_END_THRESHOLD
+    
+    if is_already_at_surface_end and \
+            !can_grab_next_surface and \
+            !just_turned_around:
+        # If we've reached the end of a surface, and we can't grab the next
+        # surface, then turn around and go the other way.
+        is_clockwise = !is_clockwise
+        return _attempt_navigation(true)
     
     var edges := []
     
-    var intra_surface_edge := _create_intra_surface_edge()
-    edges.push_back(intra_surface_edge)
+    if !is_already_at_surface_end:
+        var intra_surface_edge := IntraSurfaceEdge.new(
+                start_position_along_surface,
+                intra_surface_destination,
+                Vector2.ZERO,
+                character.movement_params)
+        edges.push_back(intra_surface_edge)
     
-    if can_grab_next_surface:
+    if can_grab_next_surface and \
+            can_leave_start_surface:
         var climb_to_neighbor_surface_edge := \
                 _create_climb_to_neighbor_surface_edge()
         edges.push_back(climb_to_neighbor_surface_edge)
-    else:
-        is_clockwise = !is_clockwise
+        
+        var intra_surface_edge := IntraSurfaceEdge.new(
+                climb_to_neighbor_surface_edge.end_position_along_surface,
+                climb_to_neighbor_surface_edge.end_position_along_surface,
+                Vector2.ZERO,
+                character.movement_params)
+        edges.push_back(intra_surface_edge)
     
     var path := PlatformGraphPath.new(edges)
     
     return character.navigator.navigate_path(path)
 
 
-func _create_intra_surface_edge() -> IntraSurfaceEdge:
+func _get_intra_surface_destination() -> PositionAlongSurface:
+    var max_distance_point := \
+            Sc.geometry.get_intersection_of_segment_and_circle(
+                    start_surface.first_point,
+                    start_surface.last_point,
+                    start_position_for_max_distance_checks,
+                    max_distance_from_start_position,
+                    true)
+    
+    var is_max_distance_point_in_the_direction_of_movement := false
+    
+    if max_distance_point != Vector2.INF:
+        var is_max_distance_point_clockwise := is_point_clockwise(
+                start_position,
+                max_distance_point,
+                start_surface)
+        is_max_distance_point_in_the_direction_of_movement = \
+                is_max_distance_point_clockwise == is_clockwise
+        
+        if !is_max_distance_point_in_the_direction_of_movement:
+            # The max-distance point is behind the character, but it's possible
+            # that there are two max distance points on this surface, so try
+            # the other point.
+            max_distance_point = \
+                    Sc.geometry.get_intersection_of_segment_and_circle(
+                            start_surface.first_point,
+                            start_surface.last_point,
+                            start_position_for_max_distance_checks,
+                            max_distance_from_start_position,
+                            false)
+            
+            is_max_distance_point_clockwise = is_point_clockwise(
+                    start_position,
+                    max_distance_point,
+                    start_surface)
+            is_max_distance_point_in_the_direction_of_movement = \
+                    is_max_distance_point_clockwise == is_clockwise
+    
+    if is_max_distance_point_in_the_direction_of_movement:
+        _reached_max_distance = true
+    
     var intra_surface_destination_target := \
+            max_distance_point if \
+            is_max_distance_point_in_the_direction_of_movement else \
             start_surface.last_point if \
             is_clockwise else \
             start_surface.first_point
-    var intra_surface_destination := PositionAlongSurfaceFactory \
+    
+    return PositionAlongSurfaceFactory \
             .create_position_offset_from_target_point(
                     intra_surface_destination_target,
                     start_surface,
                     character.movement_params.collider_half_width_height,
                     true)
-    return IntraSurfaceEdge.new(
-            start_position_along_surface,
-            intra_surface_destination,
-            Vector2.ZERO,
-            character.movement_params)
 
 
 func _create_climb_to_neighbor_surface_edge() -> ClimbToNeighborSurfaceEdge:
@@ -127,6 +190,24 @@ func _create_climb_to_neighbor_surface_edge() -> ClimbToNeighborSurfaceEdge:
                     climb_edge_jump_land_positions.land_position,
                     climb_edge_jump_land_positions.velocity_start) as \
                     ClimbToNeighborSurfaceEdge
+
+
+func is_point_clockwise(
+        basis: Vector2,
+        target: Vector2,
+        surface: Surface) -> bool:
+    match surface.side:
+        SurfaceSide.FLOOR:
+            return target.x >= basis.x
+        SurfaceSide.CEILING:
+            return target.x <= basis.x
+        SurfaceSide.LEFT_WALL:
+            return target.y >= basis.y
+        SurfaceSide.RIGHT_WALL:
+            return target.y <= basis.y
+        _:
+            Sc.logger.error()
+            return false
 
 
 #func _update_parameters() -> void:
