85c089d3f3358974d1f816fd646df5729a58c228
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-08-31 20:31:27 -0700

    Update CharacterSurfaceAnnotator surfaces to not be drawn my SurfacesAnnotator.

--- src/annotators/character_annotators/character_surface_annotator.gd
+++ src/annotators/character_annotators/character_surface_annotator.gd
@@ -10,12 +10,22 @@ var color: Color
 
 func _init(character: SurfacerCharacter) -> void:
     self.character = character
-    self.color = Sc.colors.opacify(
-            character.position_annotation_color, OPACITY)
+    self.color = Color.from_hsv(
+            character.position_annotation_color.h,
+            0.7,
+            0.9,
+            OPACITY)
 
 
 func _draw() -> void:
     if character.surface_state.is_grabbing_surface:
+        if is_instance_valid(Sc.annotators) and \
+                is_instance_valid(Sc.annotators.surfaces_annotator) and \
+                Sc.annotators.is_annotator_enabled(AnnotatorType.SURFACES):
+            color.a = Sc.annotators.surfaces_annotator.color_params.alpha_max
+        else:
+            color.a = OPACITY
+        
         Sc.draw.draw_surface(
                 self,
                 character.surface_state.grabbed_surface,
@@ -24,4 +34,8 @@ func _draw() -> void:
 
 func check_for_update() -> void:
     if character.surface_state.just_changed_surface:
+        if is_instance_valid(Sc.annotators):
+            Sc.annotators.surfaces_annotator.exclude(
+                    character.surface_state.grabbed_surface,
+                    character)
         update()
--- src/annotators/surfaces_annotator.gd
+++ src/annotators/surfaces_annotator.gd
@@ -9,9 +9,20 @@ var alpha_with_inspector_open := alpha_with_inspector_closed * 0.2
 
 var was_inspector_open := false
 
+# Dictionary<Surface, Color>
+var _surface_to_color := {}
+
+# Dictionary<Surface, ScaffolderCharacter>
+var _exclusion_surface_to_character := {}
+# Dictionary<ScaffolderCharacter, Surface>
+var _exclusion_character_to_surface := {}
+
 
 func _init(surface_parser: SurfaceParser) -> void:
     self.surface_parser = surface_parser
+    
+    for surface in surface_parser.all_surfaces:
+        _surface_to_color[surface] = color_params.get_color()
 
 
 func _process(_delta: float) -> void:
@@ -23,11 +34,13 @@ func _process(_delta: float) -> void:
 
 func _draw() -> void:
     for surface in surface_parser.all_surfaces:
+        if _exclusion_surface_to_character.has(surface):
+            continue
         _draw_surface(surface)
 
 
 func _draw_surface(surface: Surface) -> void:
-    var color := color_params.get_color()
+    var color: Color = _surface_to_color[surface]
     color.a = \
             alpha_with_inspector_open if \
             was_inspector_open else \
@@ -36,3 +49,24 @@ func _draw_surface(surface: Surface) -> void:
             self,
             surface,
             color)
+
+
+func exclude(
+        surface: Surface,
+        character: ScaffolderCharacter) -> void:
+    # Remove any stale surface-to-character mapping.
+    var previous_surface: Surface = \
+            _exclusion_character_to_surface[character] if \
+            _exclusion_character_to_surface.has(character) else \
+            null
+    if previous_surface != surface and \
+            _exclusion_surface_to_character.has(previous_surface) and \
+            _exclusion_surface_to_character[previous_surface] == character:
+        _exclusion_surface_to_character.erase(previous_surface)
+    
+    if is_instance_valid(surface):
+        _exclusion_surface_to_character[surface] = character
+    
+    _exclusion_character_to_surface[character] = surface
+    
+    update()
