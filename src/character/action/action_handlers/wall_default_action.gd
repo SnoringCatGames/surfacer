59b8368e542d34cc304c79a7902a8f06303a087b
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-09-14 13:59:14 -0700

    Fix issues with snap-to-non-floor-surface.

--- src/character/action/action_handlers/wall_default_action.gd
+++ src/character/action/action_handlers/wall_default_action.gd
@@ -22,11 +22,4 @@ func process(character) -> bool:
     character.velocity.x = 0.0
     character.velocity.y = 0.0
     
-    # Force the character horizontal position to bypass gravity and cling to
-    # the wall surface.
-    character.position.x = Sc.geometry.project_point_onto_surface_with_offset(
-            character.position,
-            character.surface_state.grabbed_surface,
-            character.movement_params.collider_half_width_height).x
-    
     return true
