01cf46da2f9b8a4ea798912ce862200a63997323
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-09-14 12:11:57 -0700

    Reconcile velocity-application-to-maintain-collisions with the move_and_slide_with_snap API.

--- src/character/action/action_handlers/all_default_action.gd
+++ src/character/action/action_handlers/all_default_action.gd
@@ -19,29 +19,4 @@ func _init().(
 func process(character) -> bool:
     character.just_triggered_jump = false
     
-    # Cancel any horizontal velocity when bumping into a wall.
-    if character.surface_state.is_touching_wall and \
-            !character.surface_state.is_triggering_wall_release:
-        # The move_and_slide system depends on maintained velocity always
-        # pushing the character into a collision, otherwise it will eventually
-        # stop the collision. If we just zero this out, move_and_slide will
-        # produce false-negatives.
-        # FIXME: ---------------- REMOVE?
-        if Su.uses_surface_normal_to_maintain_collision:
-            var wall_cling_velocity: Vector2 = \
-                    CharacterActionHandler \
-                            .MIN_SPEED_TO_MAINTAIN_HORIZONTAL_COLLISION * \
-                    Sc.time.get_combined_scale() * \
-                    -character.surface_state.grab_normal
-            character.velocity.x = wall_cling_velocity.x
-            character.velocity.y += wall_cling_velocity.y
-        else:
-            character.velocity.x = \
-                    CharacterActionHandler \
-                            .MIN_SPEED_TO_MAINTAIN_HORIZONTAL_COLLISION * \
-                    character.surface_state.toward_wall_sign / \
-                    Sc.time.get_combined_scale()
-        
-        return true
-    else:
-        return false
+    return true
--- src/character/action/action_handlers/ceiling_default_action.gd
+++ src/character/action/action_handlers/ceiling_default_action.gd
@@ -20,6 +20,7 @@ func process(character) -> bool:
     character.jump_count = 0
     character.is_rising_from_jump = false
     character.velocity.x = 0.0
+    character.velocity.y = 0.0
     
     # Force the character vertical position to bypass gravity and cling to the
     # ceiling surface.
@@ -27,21 +28,5 @@ func process(character) -> bool:
             character.position,
             character.surface_state.grabbed_surface,
             character.movement_params.collider_half_width_height).y
-    
-    # The move_and_slide system depends on some vertical gravity always pushing
-    # the character into the ceiling. If we just zero this out, move_and_slide
-    # will produce false-negatives.
-    # FIXME: ---------------- REMOVE?
-    if Su.uses_surface_normal_to_maintain_collision:
-        var cling_velocity: Vector2 = \
-                CharacterActionHandler.MIN_SPEED_TO_MAINTAIN_VERTICAL_COLLISION * \
-                Sc.time.get_combined_scale() * \
-                -character.surface_state.grab_normal
-        character.velocity.y = cling_velocity.y
-        character.velocity.x += cling_velocity.x
-    else:
-        character.velocity.y = \
-                -CharacterActionHandler.MIN_SPEED_TO_MAINTAIN_VERTICAL_COLLISION / \
-                Sc.time.get_combined_scale()
     
     return true
