5b478e7d862b31cbca7744802e9797fbf05c2836
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-09-15 14:27:52 -0700

    Start implementing project_shape_onto_surface.

--- src/utils/surfacer_geometry.gd
+++ src/utils/surfacer_geometry.gd
@@ -3,52 +3,60 @@ class_name SurfacerGeometry
 extends ScaffolderGeometry
 
 
-# Calculates where the alially-aligned surface-side-normal that goes through
+# Calculates where the axially-aligned surface-side-normal that goes through
 # the given point would intersect with the surface.
 static func project_point_onto_surface(
         point: Vector2,
         surface: Surface) -> Vector2:
-    # Check whether the point lies outside the surface boundaries.
     var start_vertex = surface.first_point
     var end_vertex = surface.last_point
-    if surface.side == SurfaceSide.FLOOR and point.x <= start_vertex.x:
-        return start_vertex
-    elif surface.side == SurfaceSide.FLOOR and point.x >= end_vertex.x:
-        return end_vertex
-    elif surface.side == SurfaceSide.CEILING and point.x >= start_vertex.x:
-        return start_vertex
-    elif surface.side == SurfaceSide.CEILING and point.x <= end_vertex.x:
-        return end_vertex
-    elif surface.side == SurfaceSide.LEFT_WALL and point.y <= start_vertex.y:
-        return start_vertex
-    elif surface.side == SurfaceSide.LEFT_WALL and point.y >= end_vertex.y:
-        return end_vertex
-    elif surface.side == SurfaceSide.RIGHT_WALL and point.y >= start_vertex.y:
-        return start_vertex
-    elif surface.side == SurfaceSide.RIGHT_WALL and point.y <= end_vertex.y:
-        return end_vertex
+    
+    # Check whether the point lies outside the surface boundaries.
+    match surface.side:
+        SurfaceSide.FLOOR:
+            if point.x <= start_vertex.x:
+                return start_vertex
+            elif point.x >= end_vertex.x:
+                return end_vertex
+        SurfaceSide.CEILING:
+            if point.x >= start_vertex.x:
+                return start_vertex
+            elif point.x <= end_vertex.x:
+                return end_vertex
+        SurfaceSide.LEFT_WALL:
+            if point.y <= start_vertex.y:
+                return start_vertex
+            elif point.y >= end_vertex.y:
+                return end_vertex
+        SurfaceSide.RIGHT_WALL:
+            if point.y >= start_vertex.y:
+                return start_vertex
+            elif point.y <= end_vertex.y:
+                return end_vertex
+        _:
+            Sc.logger.error()
+    
+    # Target lies within the surface boundaries.
+    
+    # Calculate a segment that represents the axially-aligned
+    # surface-side-normal.
+    var segment_a: Vector2
+    var segment_b: Vector2
+    if surface.side == SurfaceSide.FLOOR or \
+            surface.side == SurfaceSide.CEILING:
+        segment_a = Vector2(point.x, surface.bounding_box.position.y)
+        segment_b = Vector2(point.x, surface.bounding_box.end.y)
     else:
-        # Target lies within the surface boundaries.
-        
-        # Calculate a segment that represents the alially-aligned
-        # surface-side-normal.
-        var segment_a: Vector2
-        var segment_b: Vector2
-        if surface.side == SurfaceSide.FLOOR or \
-                surface.side == SurfaceSide.CEILING:
-            segment_a = Vector2(point.x, surface.bounding_box.position.y)
-            segment_b = Vector2(point.x, surface.bounding_box.end.y)
-        else:
-            segment_a = Vector2(surface.bounding_box.position.x, point.y)
-            segment_b = Vector2(surface.bounding_box.end.x, point.y)
-        
-        var intersection: Vector2 = \
-                Sc.geometry.get_intersection_of_segment_and_polyline(
-                        segment_a,
-                        segment_b,
-                        surface.vertices)
-        assert(intersection != Vector2.INF)
-        return intersection
+        segment_a = Vector2(surface.bounding_box.position.x, point.y)
+        segment_b = Vector2(surface.bounding_box.end.x, point.y)
+    
+    var intersection: Vector2 = \
+            Sc.geometry.get_intersection_of_segment_and_polyline(
+                    segment_a,
+                    segment_b,
+                    surface.vertices)
+    assert(intersection != Vector2.INF)
+    return intersection
 
 
 # Projects the given point onto the given surface, then offsets the point away
@@ -82,72 +90,17 @@ static func get_surface_normal_at_point(
     if !is_instance_valid(surface):
         return Vector2.INF
     
-    var epsilon := 0.01
-    
-    var vertices := surface.vertices
-    var count := vertices.size()
-    
-    if count <= 1:
+    if surface.vertices.size() <= 1:
         return surface.normal
     
-    var segment_start := Vector2.INF
-    var segment_end := Vector2.INF
-    
-    match surface.side:
-        SurfaceSide.FLOOR:
-            if point.x < vertices[0].x + epsilon:
-                segment_start = vertices[0]
-                segment_end = vertices[1]
-            elif point.x > vertices[count - 1].x - epsilon:
-                segment_start = vertices[count - 2]
-                segment_end = vertices[count - 1]
-            else:
-                for i in range(1, count):
-                    if point.x < vertices[i].x:
-                        segment_start = vertices[i - 1]
-                        segment_end = vertices[i]
-                        break
-        SurfaceSide.LEFT_WALL:
-            if point.y < vertices[0].y + epsilon:
-                segment_start = vertices[0]
-                segment_end = vertices[1]
-            elif point.y > vertices[count - 1].y - epsilon:
-                segment_start = vertices[count - 2]
-                segment_end = vertices[count - 1]
-            else:
-                for i in range(1, count):
-                    if point.y < vertices[i].y:
-                        segment_start = vertices[i - 1]
-                        segment_end = vertices[i]
-                        break
-        SurfaceSide.RIGHT_WALL:
-            if point.y > vertices[0].y - epsilon:
-                segment_start = vertices[0]
-                segment_end = vertices[1]
-            elif point.y < vertices[count - 1].y + epsilon:
-                segment_start = vertices[count - 2]
-                segment_end = vertices[count - 1]
-            else:
-                for i in range(1, count):
-                    if point.y > vertices[i].y:
-                        segment_start = vertices[i - 1]
-                        segment_end = vertices[i]
-                        break
-        SurfaceSide.CEILING:
-            if point.x > vertices[0].x - epsilon:
-                segment_start = vertices[0]
-                segment_end = vertices[1]
-            elif point.x < vertices[count - 1].x + epsilon:
-                segment_start = vertices[count - 2]
-                segment_end = vertices[count - 1]
-            else:
-                for i in range(1, count):
-                    if point.x > vertices[i].x:
-                        segment_start = vertices[i - 1]
-                        segment_end = vertices[i]
-                        break
-        _:
-            Sc.logger.error()
+    var segment_points_result := []
+    get_surface_segment_at_point(
+            segment_points_result,
+            surface,
+            point,
+            true)
+    var segment_start: Vector2 = segment_points_result[0]
+    var segment_end: Vector2 = segment_points_result[1]
     
     var displacement := segment_end - segment_start
     # Displacement is clockwise around convex surfaces, so the normal is the
@@ -156,6 +109,201 @@ static func get_surface_normal_at_point(
     var normal := perpendicular.normalized()
     
     return normal
+
+
+# FIXME: LEFT OFF HERE: ----------------------------------
+# - Call project_shape_onto_surface.
+# - Some of the call sites:
+#   - IntraSurfaceEdge/call-sites
+#   - ClimbToAdjacentSurfaceCalculator/Edge
+#   - find_closest_positions_on_surfaces
+#   - find_closest_position_on_a_surface
+#   - Search for usages of project_point_onto_surface_with_offset?
+#   - Search for usages of create_position_offset_from_target_point?
+#   - Search for usages of match_surface_target_and_collider?
+
+
+# -   Calculates where the center position of the given shape would be if it
+#     were moved along the axially-aligned surface-side-normal until it just
+#     rested against the given surface.
+# -   This only considers a maximum of two segments along the surface polyline.
+#     -   Most surfaces should be created with long enough segments, relative
+#         to character sizes, that this shouldn't be a problem.
+static func project_shape_onto_surface(
+        start_position: Vector2,
+        shape: Shape2D,
+        is_rotated_90: bool,
+        half_width_height: Vector2,
+        surface: Surface) -> Vector2:
+    
+    
+    
+    
+    
+    
+    
+    if !is_instance_valid(surface):
+        return Vector2.INF
+    
+    if surface.vertices.size() <= 1:
+        # FIXME: LEFT OFF HERE: --------------------------------
+        return Vector2.INF
+    
+    
+    
+    
+    
+    
+    
+    var segment_points_result := []
+    
+    # FIXME: LEFT OFF HERE: ---------------
+    # - Call this for both the min and max side of the shape.
+    # - If both segments are the same, then ignore the second.
+    # - Then, run the shape projection for both segments, and keep whichever
+    #   result is further-out from the surface.
+#    get_surface_segment_at_point(
+#            segment_points_result,
+#            surface,
+#            ,
+#            false)
+    
+    
+    
+    
+    
+    if shape is CircleShape2D:
+        pass
+    elif shape is CapsuleShape2D:
+        pass
+    elif shape is RectangleShape2D:
+        pass
+    else:
+        Sc.logger.error()
+        return Vector2.INF
+    
+    
+    
+    
+    
+    
+    # FIXME: LEFT OFF HERE: --------------------------------
+    return Vector2.INF
+
+
+# -   Calculates where the center position of the given shape would be if it
+#     were moved along the given axially-aligned normal until it just rested
+#     against the given segment.
+static func project_shape_onto_segment(
+        start_position: Vector2,
+        shape: Shape2D,
+        is_rotated_90: bool,
+        segment_start: Vector2,
+        segment_end: Vector2) -> Vector2:
+    # FIXME: LEFT OFF HERE: --------------------------------
+    return Vector2.INF
+
+
+# -   Finds the end points of the segment along the given surface that the
+#     axially-aligned projection of the given point onto the surface would
+#     intersect.
+static func get_surface_segment_at_point(
+        segment_points_result: Array,
+        surface: Surface,
+        point: Vector2,
+        uses_end_segment_if_outside_bounds := true) -> void:
+    if !is_instance_valid(surface):
+        segment_points_result.resize(0)
+        return
+    
+    var epsilon := 0.01
+    
+    var vertices := surface.vertices
+    var count := vertices.size()
+    
+    if count <= 1:
+        segment_points_result.resize(0)
+        return
+    
+    var inside_bounds := false
+    var segment_start := Vector2.INF
+    var segment_end := Vector2.INF
+    
+    match surface.side:
+        SurfaceSide.FLOOR:
+            if point.x < vertices[0].x + epsilon:
+                inside_bounds = false
+                segment_start = vertices[0]
+                segment_end = vertices[1]
+            elif point.x > vertices[count - 1].x - epsilon:
+                inside_bounds = false
+                segment_start = vertices[count - 2]
+                segment_end = vertices[count - 1]
+            else:
+                inside_bounds = true
+                for i in range(1, count):
+                    if point.x < vertices[i].x:
+                        segment_start = vertices[i - 1]
+                        segment_end = vertices[i]
+                        break
+        SurfaceSide.LEFT_WALL:
+            if point.y < vertices[0].y + epsilon:
+                inside_bounds = false
+                segment_start = vertices[0]
+                segment_end = vertices[1]
+            elif point.y > vertices[count - 1].y - epsilon:
+                inside_bounds = false
+                segment_start = vertices[count - 2]
+                segment_end = vertices[count - 1]
+            else:
+                inside_bounds = true
+                for i in range(1, count):
+                    if point.y < vertices[i].y:
+                        segment_start = vertices[i - 1]
+                        segment_end = vertices[i]
+                        break
+        SurfaceSide.RIGHT_WALL:
+            if point.y > vertices[0].y - epsilon:
+                inside_bounds = false
+                segment_start = vertices[0]
+                segment_end = vertices[1]
+            elif point.y < vertices[count - 1].y + epsilon:
+                inside_bounds = false
+                segment_start = vertices[count - 2]
+                segment_end = vertices[count - 1]
+            else:
+                inside_bounds = true
+                for i in range(1, count):
+                    if point.y > vertices[i].y:
+                        segment_start = vertices[i - 1]
+                        segment_end = vertices[i]
+                        break
+        SurfaceSide.CEILING:
+            if point.x > vertices[0].x - epsilon:
+                inside_bounds = false
+                segment_start = vertices[0]
+                segment_end = vertices[1]
+            elif point.x < vertices[count - 1].x + epsilon:
+                inside_bounds = false
+                segment_start = vertices[count - 2]
+                segment_end = vertices[count - 1]
+            else:
+                inside_bounds = true
+                for i in range(1, count):
+                    if point.x > vertices[i].x:
+                        segment_start = vertices[i - 1]
+                        segment_end = vertices[i]
+                        break
+        _:
+            Sc.logger.error()
+    
+    if inside_bounds or \
+            uses_end_segment_if_outside_bounds:
+        segment_points_result.resize(2)
+        segment_points_result[0] = segment_start
+        segment_points_result[1] = segment_end
+    else:
+        segment_points_result.resize(0)
 
 
 static func are_position_wrappers_equal_with_epsilon(
