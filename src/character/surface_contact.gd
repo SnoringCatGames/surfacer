209f07aeb04e0b7505df684965ae344025eb28f0
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-11-09 12:44:31 -0800

    Fix an issue with how is-rounding-corner surface-state logic would incorrectly assume the contact point is the corner point, even if the surface is not axially aligned, and the character doesn't touch the corner; Add additional verbose logs for debugging is-rounding-corner logic; Add a new flag to SurfaceContact for tracking which mechanism caused the contact to exist;

--- src/character/surface_contact.gd
+++ src/character/surface_contact.gd
@@ -2,6 +2,18 @@ class_name SurfaceContact
 extends Reference
 
 
+enum {
+    UNKNOWN,
+    PHYSICS,
+    MATCH_ROUNDING_CORNER,
+    MATCH_TRAJECTORY,
+    INITIAL_ATTACHMENT,
+}
+
+
+## This indicates what mechanism led to the creation of this contact.
+var type := UNKNOWN
+
 var surface: Surface
 var contact_position := Vector2.INF
 var contact_normal := Vector2.INF
