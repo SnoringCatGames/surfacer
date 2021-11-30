e4243654eccbde1108969431d17114574fc78412
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-11-30 13:49:24 -0800

    Fix default velocity-cap to account for intra_surface_edge_speed_multiplier.

--- src/character/action/action_handlers/cap_velocity_action.gd
+++ src/character/action/action_handlers/cap_velocity_action.gd
@@ -17,8 +17,19 @@ func _init().(
 
 
 func process(character) -> bool:
+    var max_horizontal_speed: float
+    if character.current_max_horizontal_speed == \
+            character.movement_params.max_horizontal_speed_default and \
+            character.surface_state.is_grabbing_surface:
+        max_horizontal_speed = \
+                character.movement_params.max_horizontal_speed_default * \
+                character.movement_params.intra_surface_edge_speed_multiplier
+    else:
+        max_horizontal_speed = character.current_max_horizontal_speed
+    
     character.velocity = MovementUtils.cap_velocity(
             character.velocity,
             character.movement_params,
-            character.current_max_horizontal_speed)
+            max_horizontal_speed)
+    
     return true
