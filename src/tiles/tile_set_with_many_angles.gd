7db16a751e78b4110f1b6c4200f898fca9eb83bb
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-11-30 14:31:24 -0800

    Refactor how tile-set properties are configured.

--- src/tiles/tile_set_with_many_angles.gd
+++ src/tiles/tile_set_with_many_angles.gd
@@ -3,36 +3,15 @@ class_name TileSetWithManyAngles
 extends SurfacesTileSet
 
 
-const _TILE_IDS := [
-    "1_tile_with_90s",
-    "2_tile_with_45s",
-    "3_tile_with_27s_floors_pos",
-    "4_tile_with_27s_floors_neg",
-    "5_tile_with_27s_ceilings_pos",
-    "6_tile_with_27s_ceilings_neg",
-    "7_tile_with_halves",
-    "8_tile_with_odd_joins",
-    "9_tile_with_corner_caps",
+const _PROPERTIES_MANIFEST := [
+    # TODO: Use separate inclusion lists for assigning non-default properties
+    #       for certain tile IDs.
+    ["disabled", []],
 ]
 
-const _DISABLED_SURFACES_TILE_IDS := {
-}
 
-
-func _get_tile_manifest() -> Dictionary:
-    var manifest := {}
-    for tile_id in _TILE_IDS:
-        var tile_config := {}
-        # TODO: Use separate inclusion/exclusion lists for assigning non-default
-        #       properties for certain tile IDs.
-        if _DISABLED_SURFACES_TILE_IDS.has(tile_id):
-            tile_config.surface_properties = \
-                    Su.surface_properties.properties["disabled"]
-        else:
-            tile_config.surface_properties = \
-                    Su.surface_properties.properties["default"]
-        manifest[tile_id] = tile_config
-    return manifest
+func _init().(_PROPERTIES_MANIFEST) -> void:
+    pass
 
 
 func _is_tile_bound( \
--- src/tiles/tile_set_with_square_corners.gd
+++ src/tiles/tile_set_with_square_corners.gd
@@ -1,14 +1,3 @@
 tool
 class_name TileSetWithSquareCorners
 extends SurfacesTileSet
-
-
-func _get_tile_manifest() -> Dictionary:
-    var manifest := {}
-    for tile_id in get_tiles_ids():
-        var tile_name := tile_get_name(tile_id)
-        var tile_config := {}
-        tile_config.surface_properties = \
-                Su.surface_properties.properties["default"]
-        manifest[tile_name] = tile_config
-    return manifest
