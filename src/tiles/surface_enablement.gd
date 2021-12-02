c05e6548a4d9f60d58e9e2dd48e7404e35b12198
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-12-02 15:43:45 -0800

    Create a surface_enablement editor icon.

--- src/tiles/surface_enablement.gd
+++ src/tiles/surface_enablement.gd
@@ -1,5 +1,5 @@
 tool
 class_name SurfaceEnablement, \
-"res://addons/surfacer/assets/images/editor_icons/surfaces_tile_map.png"
+"res://addons/surfacer/assets/images/editor_icons/surface_enablement.png"
 extends SurfaceMark
 ## For marking surfaces that can or cannot be accessed.
