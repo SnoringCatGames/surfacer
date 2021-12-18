c750c8566c987392c1d333e24d001fd7a4b7407b
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-12-18 14:52:22 -0800

    Update calculate_velocity_for_displacement to support starting with max-speed but decelerating.

--- src/platform_graph/edge/calculators/edge_calculator.gd
+++ src/platform_graph/edge/calculators/edge_calculator.gd
@@ -455,6 +455,7 @@ static func _optimize_edge_jump_position_for_floor(
                         previous_velocity_end_x,
                         acceleration_x,
                         max_speed,
+                        true,
                         true)
         var velocity_start_y := movement_params.jump_boost
         var velocity_start = Vector2(velocity_start_x, velocity_start_y)
