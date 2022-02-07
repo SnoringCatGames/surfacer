e7e516ad8a52f4a193a4711cfce67792e656ab34
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-02-07 14:27:01 -0800

    Continue prototyping a new tile-set system, with custom binding logic for odd angles.

--- src/tiles/subtile_target_corner_utils.gd
+++ src/tiles/subtile_target_corner_utils.gd
@@ -5,12 +5,9 @@ extends Reference
 static func get_target_top_left_corner(proximity: CellProximity) -> int:
     if proximity.is_top_empty:
         if proximity.is_left_empty:
-            if proximity.is_angle_type_45:
-                # FIXME: LEFT OFF HERE: ------------------
-                pass
-            elif proximity.is_angle_type_27:
-                # FIXME: LEFT OFF HERE: -------- A27
-                pass
+            if proximity.is_angle_type_45 or \
+                    proximity.is_angle_type_27:
+                return SubtileCorner.EMPTY
             else:
                 return SubtileCorner.EXT_90_90_CONVEX
         else:
@@ -81,7 +78,110 @@ static func get_target_top_left_corner(proximity: CellProximity) -> int:
                             "Clipped corner",
                             proximity)
             else:
-                # Adjacent sides and corner are present.
+                # Internal(ish): Adjacent sides and corner are present.
+                if proximity.is_bottom_empty:
+                    if proximity.is_right_empty:
+                        # Opposite sides are empty.
+                        if proximity.is_angle_type_45:
+                            return SubtileCorner.EXT_INT_45_CLIPPED
+                        elif proximity.is_angle_type_27:
+                            # FIXME: LEFT OFF HERE: -------- A27
+                            pass
+                        else:
+                            return SubtileCorner.EXT_INT_90_90_CONVEX
+                    else:
+                        # Adjacent sides and corner and opposite horizontal
+                        # side are present.
+                        if proximity.is_top_right_empty:
+                            if proximity.get_is_top_right_corner_clipped_90_90() or \
+                                    proximity.get_is_top_right_corner_clipped_90V_45():
+                                if proximity.get_is_45_neg_ceiling(-1,0):
+                                    return SubtileCorner.EXT_INT_90V_45_CONVEX_ACUTE
+                                else:
+                                    return SubtileCorner.EXT_INT_90_90_CONVEX
+                            elif proximity.get_is_top_right_corner_clipped_45_45() or \
+                                    proximity.get_is_top_right_corner_clipped_90H_45():
+                                if proximity.get_is_45_neg_ceiling(-1,0):
+                                    return SubtileCorner.EXT_INT_45_FLOOR_45_CEILING
+                                else:
+                                    return SubtileCorner.EXT_INT_90H_45_CONVEX_ACUTE
+                            else:
+                                # FIXME: LEFT OFF HERE: -------- A27
+                                pass
+                        else:
+                            if proximity.get_is_45_neg_ceiling(-1,0):
+                                return SubtileCorner.EXT_INT_90H_45_CONVEX
+                            else:
+                                return SubtileCorner.EXT_INT_90H
+                else:
+                    if proximity.is_right_empty:
+                        # Adjacent sides and corner and opposite vertical side
+                        # are present.
+                        if proximity.is_bottom_left_empty:
+                            if proximity.get_is_bottom_left_corner_clipped_90_90() or \
+                                    proximity.get_is_bottom_left_corner_clipped_90H_45():
+                                if proximity.get_is_45_neg_floor(0,-1):
+                                    return SubtileCorner.EXT_INT_90H_45_CONVEX
+                                else:
+                                    return SubtileCorner.EXT_INT_90_90_CONVEX
+                            elif proximity.get_is_bottom_left_corner_clipped_45_45() or \
+                                    proximity.get_is_bottom_left_corner_clipped_90V_45():
+                                if proximity.get_is_45_neg_floor(0,-1):
+                                    return SubtileCorner.EXT_INT_45_FLOOR_45_CEILING
+                                else:
+                                    return SubtileCorner.EXT_INT_90V_45_CONVEX
+                            else:
+                                # FIXME: LEFT OFF HERE: -------- A27
+                                pass
+                        else:
+                            if proximity.get_is_45_neg_floor(0,-1):
+                                return SubtileCorner.EXT_INT_90V_45_CONVEX
+                            else:
+                                return SubtileCorner.EXT_INT_90V
+                    else:
+                        # All sides and adjacent corner are present.
+                        if proximity.is_top_right_empty:
+                            if proximity.is_bottom_left_empty:
+                                if proximity.is_bottom_right_empty:
+                                    # FIXME: LEFT OFF HERE: --------------------
+                                    pass
+                                else:
+                                    # FIXME: LEFT OFF HERE: --------------------
+                                    pass
+                            else:
+                                if proximity.is_bottom_right_empty:
+                                    # FIXME: LEFT OFF HERE: --------------------
+                                    pass
+                                else:
+                                    # FIXME: LEFT OFF HERE: --------------------
+                                    pass
+                        else:
+                            if proximity.is_bottom_left_empty:
+                                if proximity.is_bottom_right_empty:
+                                    # FIXME: LEFT OFF HERE: --------------------
+                                    pass
+                                else:
+                                    # FIXME: LEFT OFF HERE: --------------------
+                                    pass
+                            else:
+                                if proximity.is_bottom_right_empty:
+                                    # FIXME: LEFT OFF HERE: --------------------
+                                    pass
+                                else:
+                                    # FIXME: LEFT OFF HERE: --------------------
+                                    pass
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
                 
                 # FIXME: LEFT OFF HERE: ---------------------------------------
                 
