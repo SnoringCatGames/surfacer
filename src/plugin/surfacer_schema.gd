23a3c17e431a82db789151a9a41ddf9eb8f1fe80
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-21 13:02:39 -0700

    Start adding support for toggling various manifest modes within the main-screen plugin.

--- src/plugin/surfacer_schema.gd
+++ src/plugin/surfacer_schema.gd
@@ -8,7 +8,9 @@ const _FOLDER_NAME := "surfacer"
 const _AUTO_LOAD_NAME := "Su"
 const _AUTO_LOAD_DEPS := ["Sc"]
 const _AUTO_LOAD_PATH := "res://addons/surfacer/src/config/su.gd"
-const _PLUGIN_ICON_DIRECTORY_PATH := "res://addons/surfacer/assets/images/editor_icons/plugin/"
+const _MANIFEST_PATH := "res://addons/surfacer/src/config/manifest.json"
+const _PLUGIN_ICON_DIRECTORY_PATH := \
+        "res://addons/surfacer/assets/images/editor_icons/plugin/"
 
 const _surfacer_debug_params := {}
 
@@ -219,6 +221,7 @@ func _init().(
         _AUTO_LOAD_NAME,
         _AUTO_LOAD_DEPS,
         _AUTO_LOAD_PATH,
+        _MANIFEST_PATH,
         _PLUGIN_ICON_DIRECTORY_PATH,
         _PROPERTIES) -> void:
     pass
