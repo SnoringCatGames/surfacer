11d29e75f46fb5233cce4183c27f6c42a56396f7
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-23 10:03:01 -0700

    Move plugin files into a separate subdirectory; Create a new autoload for plugin-specific stuff; Decouple FrameworkPlugin from any transitive dependencies on FrameworkGlobal types.

--- src/plugin/surfacer_plugin.gd
+++ src/plugin/surfacer_plugin.gd
@@ -3,8 +3,13 @@ class_name SurfacerPlugin
 extends FrameworkPlugin
 
 
-const _SCHEMA_CLASS := SurfacerSchema
+const _AUTO_LOAD_NAME := "Su"
+const _AUTO_LOAD_PATH := "res://addons/surfacer/src/config/su.gd"
+const _SCHEMA_PATH := "res://addons/surfacer/src/plugin/surfacer_schema.gd"
 
 
-func _init().(_SCHEMA_CLASS) -> void:
+func _init().(
+        _AUTO_LOAD_NAME,
+        _AUTO_LOAD_PATH,
+        _SCHEMA_PATH) -> void:
     pass
