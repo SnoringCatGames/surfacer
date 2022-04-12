55578b313bae6c73ab7ea5b58a22ea83de36e77b
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-04-12 12:07:37 -0700

    Implement SwipePanController.

--- src/level/swipe_pan_controller.gd
+++ src/level/swipe_pan_controller.gd
@@ -3,9 +3,8 @@ extends CameraPanController
 
 
 # FIXME: LEFT OFF HERE: --------------------------------------
-# - TIE INTO Sc.level.pointer_listener.
-#   - I might need to add support for multi-touch to this?
 # 
+# - Implement zoom/pan boundaries, and standardize with other controller.
 # - Implement touch-based drag pan.
 # - Add momentum and friction to drag.
 # - Implement multi-touch pinch-zoom.
@@ -18,7 +17,11 @@ extends CameraPanController
 # - Sc.level.surfaces_bounds
 
 
+const _PAN_SPEED_MULTIPLIER := 1.5
+const _ZOOM_SPEED_MULTIPLIER := 1.8
 
+var _drag_start_position := Vector2.INF
+var _zoom_start_distance := INF
 
 
 func _init(previous_pan_controller: CameraPanController = null).(
@@ -38,25 +41,46 @@ func _validate() -> void:
     assert(Sc.gui.is_player_interaction_enabled)
 
 
-func _on_dragged(pointer_position: Vector2) -> void:
+func _reset() -> void:
+    self._drag_start_position = Vector2.INF
+    self._zoom_start_distance = INF
+
+
+func _on_dragged(
+        pointer_screen_position: Vector2,
+        pointer_level_position: Vector2) -> void:
     if Sc.level.pointer_listener.get_is_control_pressed():
+        _reset()
         return
-    # FIXME: LEFT OFF HERE: --------------------------------------
-    pass
+    var offset: Vector2 = \
+            _target_offset - \
+            Sc.level.pointer_listener.current_drag_screen_displacement * \
+            _PAN_SPEED_MULTIPLIER * \
+            Sc.camera.controller.get_zoom()
+    _update_camera(offset, _target_zoom)
 
 
-func _on_released(pointer_position: Vector2) -> void:
+func _on_released(
+        pointer_screen_position: Vector2,
+        pointer_level_position: Vector2) -> void:
     if Sc.level.pointer_listener.get_is_control_pressed():
+        _reset()
         return
-    # FIXME: LEFT OFF HERE: --------------------------------------
-    pass
+    _reset()
 
 
-func _on_pinch_changed(pinch_distance: float, pinch_angle: float) -> void:
-    # FIXME: LEFT OFF HERE: --------------------------------------
-    pass
+func _on_pinch_changed(
+        pinch_distance: float,
+        pinch_angle: float) -> void:
+    var distance_ratio := \
+            Sc.level.pointer_listener.current_pinch_distance_ratio_from_previous
+    if distance_ratio > 1.0:
+        distance_ratio = 1.0 + (distance_ratio - 1.0) * _ZOOM_SPEED_MULTIPLIER
+    else:
+        distance_ratio = 1.0 - (1.0 - distance_ratio) * _ZOOM_SPEED_MULTIPLIER
+    var zoom: float = _target_zoom / distance_ratio
+    _update_camera(_target_offset, zoom)
 
 
 func _on_pinch_finished() -> void:
-    # FIXME: LEFT OFF HERE: --------------------------------------
-    pass
+    _reset()
