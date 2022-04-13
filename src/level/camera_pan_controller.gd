89016d864e4a773eb5af572dac491f32a13d6abe
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-04-13 10:30:20 -0700

    Fix camera-pan-controller clamping zoom and pan to the level's camera-bounds; Move scroll-to-zoom functionality out of camera-controller (as a manual override) and into camera-pan-controller (as a standard, limitted control); Refactor scroll-to-zoom functionality to zoom into cursor position rather than the current camera center position; Adjust max-zoom limit;

--- src/level/camera_pan_controller.gd
+++ src/level/camera_pan_controller.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 
 const _TWEEN_DURATION := 0.1
-const _MIN_CAMERA_ZOOM := 0.00001
+const _MIN_CAMERA_ZOOM := 0.01
 
 var _tween: ScaffolderTween
 
@@ -45,6 +45,32 @@ func reset() -> void:
     _update_camera(Vector2.ZERO, 1.0)
 
 
+func _unhandled_input(event: InputEvent) -> void:
+    # Mouse wheel events are never considered pressed by Godot--rather they are
+    # only ever considered to have just happened.
+    if Sc.gui.is_player_interaction_enabled and \
+            event is InputEventMouseButton:
+        if event.button_index == BUTTON_WHEEL_UP or \
+                event.button_index == BUTTON_WHEEL_DOWN:
+            # Zoom toward the cursor.
+            var zoom := \
+                    _target_zoom / CameraController._MANUAL_ZOOM_STEP_RATIO if \
+                    event.button_index == BUTTON_WHEEL_UP else \
+                    _target_zoom * CameraController._MANUAL_ZOOM_STEP_RATIO
+            
+            var cursor_level_position: Vector2 = \
+                    Sc.utils.get_level_touch_position(event)
+            var camera_level_position: Vector2 = \
+                    _get_camera_parent_position() + Sc.camera.controller.offset
+            var cursor_camera_position := \
+                    cursor_level_position - camera_level_position
+            var delta_offset := \
+                    cursor_camera_position * (1 - zoom / _target_zoom)
+            var offset := _target_offset + delta_offset
+            
+            _update_camera(offset, zoom)
+
+
 func _update_camera(
         next_offset: Vector2,
         next_zoom: float) -> void:
@@ -52,26 +78,30 @@ func _update_camera(
     var previous_target_zoom := _target_zoom
     
     # Ensure the pan-controller keeps the camera in-bounds.
-    next_zoom = clamp(next_zoom, _MIN_CAMERA_ZOOM, _max_zoom_for_camera_bounds)
+    var other_zoom_factor: float = \
+            Sc.camera.controller.get_zoom() / \
+            Sc.camera.controller._manual_zoom / \
+            Sc.camera.controller._camera_pan_controller_zoom
+    next_zoom = clamp(
+            next_zoom,
+            _MIN_CAMERA_ZOOM / other_zoom_factor,
+            _max_zoom_for_camera_bounds / other_zoom_factor)
+    var accountable_camera_zoom := next_zoom * other_zoom_factor
     var camera_position_without_pan_controller: Vector2 = \
             _get_camera_parent_position() + \
             Sc.camera.controller.offset - \
+            Sc.camera.controller._manual_offset - \
             Sc.camera.controller._camera_pan_controller_offset
     var min_offset := \
-            _calculate_min_position_for_zoom_for_camera_bounds(next_zoom) - \
+            _calculate_min_position_for_zoom_for_camera_bounds(
+                accountable_camera_zoom) - \
             camera_position_without_pan_controller
     var max_offset := \
-            _calculate_max_position_for_zoom_for_camera_bounds(next_zoom) - \
+            _calculate_max_position_for_zoom_for_camera_bounds(
+                accountable_camera_zoom) - \
             camera_position_without_pan_controller
     next_offset.x = clamp(next_offset.x, min_offset.x, max_offset.x)
     next_offset.y = clamp(next_offset.y, min_offset.y, max_offset.y)
-    
-    # FIXME: --------------------
-#    Sc.logger.print(">>>>>>>>>>>>>>>>>>>>>")
-#    Sc.logger.print(str(Sc.level.camera_bounds))
-#    Sc.logger.print(str(Sc.camera.controller.get_camera().get_viewport_rect().size * next_zoom))
-#    Sc.logger.print(str(Sc.camera.controller.get_visible_region()))
-#    Sc.logger.print("%s, %s, %s, %s" % [str(next_zoom), str(next_offset), str(min_offset), str(max_offset)])
     
     _target_offset = next_offset
     _target_zoom = next_zoom
