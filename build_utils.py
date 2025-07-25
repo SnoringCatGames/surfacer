07b8139f3fbd7d85003e341963772b1044af628b
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2025-07-25 14:14:24 -0700

    Fix build.

--- build_utils.py
+++ build_utils.py
@@ -20,6 +20,10 @@ def set_up(
         print_error("submodules/snore_core must be a submodule of the root repository.")
         sys.exit(1)
 
-    src_path = is_setup_for_self and "src/" or surfacer_addon_dir_name + "/src/"
+    src_path = (
+        is_setup_for_self
+        and "src/"
+        or "submodules/{}/src/".format(surfacer_addon_dir_name)
+    )
     cpp_paths.extend([src_path])
-    sources.extend(glob.glob(src_path + "**/*.cpp", recursive=True))
+    sources.extend(glob.glob("{}**/*.cpp".format(src_path), recursive=True))
